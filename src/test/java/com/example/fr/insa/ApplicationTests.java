a023f36fc77fb08cd6a5f4df0ac1e1c1d3c3f666
Antoine <antbury28@gmail.com>
2021-11-27 10:30:20 +0100

    Ajout du projet Spring Boot

---
+++ src/test/java/com/example/fr/insa/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fr.insa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
