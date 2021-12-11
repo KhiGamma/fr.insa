0b728e8d3b66dc4d67818965b0f72e233129df57
ArnaudJuilien <61822174+ArnaudJuilien@users.noreply.github.com>
2021-12-11 11:59:21 +0100

    gestion des transactions

--- src/main/java/com/example/fr/insa/ressources/TransactionRessource.java
+++ src/main/java/com/example/fr/insa/ressources/TransactionRessource.java
@@ -1,7 +1,19 @@
 package com.example.fr.insa.ressources;
 
+import com.example.fr.insa.exceptions.FonctionnalProcessException;
+import com.example.fr.insa.models.Transaction;
+import com.example.fr.insa.ressources.dto.TransactionCreateModel;
 import com.example.fr.insa.services.TransactionService;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +23,27 @@ public class TransactionRessource {
 
     @Autowired
     private TransactionService transactionService;
+    
+    @GetMapping()
+    public List<Transaction> getAllTransaction() {
+        return this.transactionService.getAllTransaction();
+    }
+
+    @GetMapping("{id}")
+    public Transaction getTransaction(@PathVariable("id") int id) throws Exception {
+        return transactionService.getTransactionById(id);
+    }
+
+    @PostMapping
+    public Transaction createTransaction(@RequestBody TransactionCreateModel transactionToCreate) throws FonctionnalProcessException {
+        return this.transactionService.saveTransaction(transactionToCreate);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity deleteTransaction(@PathVariable("id") int id) {
+        this.transactionService.deleteTransaction(id);
+        return ResponseEntity.ok().build();
+    }
+
 
 }
