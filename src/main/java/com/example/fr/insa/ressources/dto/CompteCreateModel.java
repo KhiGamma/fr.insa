cea5bdae77af6b75cef4f7faf6e4fd90e452e7d1
ArnaudJuilien <61822174+ArnaudJuilien@users.noreply.github.com>
2021-12-11 10:58:45 +0100

    gestion des comptes

---
+++ src/main/java/com/example/fr/insa/ressources/dto/CompteCreateModel.java
@@ -0,0 +1,14 @@
+package com.example.fr.insa.ressources.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CompteCreateModel {
+	
+	private float soldeCompte;
+
+    private float decouvert;
+
+}
